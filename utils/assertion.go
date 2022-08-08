78eb8f52d014bcac8ab397f72acc270ad4210227
mervyn <feimei.zhan@live.com>
2022-08-08 10:37:03 +0800

    add new utils of assertion

--- utils/assertion.go
+++ utils/assertion.go
@@ -39,3 +39,12 @@ func AssertEqual2[K any, P1 any, P2 any](what string, expected K, fn func(P1, P2
 	}
 	fmt.Printf("%s succeeded\n", what)
 }
+
+func AssertEqual3[K any, P1 any, P2 any, P3 any](what string, expected K, fn func(P1, P2, P3) K, p1 P1, p2 P2, p3 P3) {
+	defer Elapsed(what)()
+	if diff := deep.Equal(fn(p1, p2, p3), expected); diff != nil {
+		emsg := fmt.Sprintf("%s error: %v\n", what, diff)
+		panic(emsg)
+	}
+	fmt.Printf("%s succeeded\n", what)
+}
